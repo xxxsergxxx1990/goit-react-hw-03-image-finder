65debbf4a56c832ac332d9ee0d0a437a8b0e1e92
Sergey Solodovnyk <xxxsergxxx1990@gmail.com>
2023-11-12 11:59:18 +0200

    ..

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,16 @@
-import {Component} from 'react'
-
-
-
-
-
-
-
-
-export class App extends Component{
-state = {
-
-}
-
-
-render(){
-
-
-
-  return
-}
-
-}
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
+  );
+};
